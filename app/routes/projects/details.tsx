7ee26b35273df5dad68dff6941c4a8ff5525c7d3
Willice Opalo <williceopalo11@gmail.com>
2025-09-27 01:03:40 +0300

    Projects details item

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,5 +1,33 @@
-const ProjectDetailPage = () => {
-    return ( <><h1 className="text-gray-400 text-xl">project detail</h1></> );
+import { Link } from "react-router";
+import type { Route } from "./+types/details";
+import type { Project } from "~/types";
+import { FaArrowLeft } from "react-icons/fa";
+
+export async function loader({ request, params }: Route.LoaderArgs): Promise<Project> {
+    const res = await fetch(`http://localhost:8000/projects/${params.id}`)
+    if(!res.ok) throw new Response('Project not found', {status:404})
+    const project: Project = await res.json()
+    return project
+}
+
+const ProjectDetailPage = ({ loaderData }: Route.ComponentProps) => {
+    const project = loaderData;
+    return (
+        <>
+            <Link to={'/projects'} className="flex items-center text-blue-400 hover:text-blue-500 mb-6 transition">
+                <FaArrowLeft className="mr-2" /> Back To Projects
+            </Link>
+
+            <div className="grid gap-8 md:grid-cols-2 item-start">
+                <div>
+                    <img src={project.image} alt={project.title} className="w-full rounded-lg shadow-md"/>
+                </div>
+                <h1 className="text-3xl font-bold text-blue-400 mb-4">
+                    {project.title}
+                </h1>
+            </div>
+        </>
+    );
 }
  
 export default ProjectDetailPage;
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -5,12 +5,13 @@ import type { Project } from "~/types";
 export async function loader({ request }: Route.LoaderArgs): Promise<{projects: Project[]}>{
     const res = await fetch('http://localhost:8000/projects')
     const data = await res.json()
+    // console.log(data)
     return {projects: data}
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
     const { projects } = loaderData as {projects: Project[]}
-    console.log(projects)
+    // console.log(projects)
     return ( 
         <>
             <h2 className="text-3xl text-white font-bold mb-8">
